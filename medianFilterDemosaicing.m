d98d7351e37be2227c33112c8e1011bdf072c3fb
simplay <michael.single@students.unibe.ch>
2014-09-29 00:12:04 +0200

    Fixing bayern filtering

---
+++ medianFilterDemosaicing.m
@@ -0,0 +1,7 @@
+function [ output_args ] = medianFilterDemosaicing( input_args )
+%MEDIANFILTERDEMOSAICING Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
--- start.m
+++ start.m
@@ -22,45 +22,8 @@ title('Inverted image');
 %% Assignement 2.1
 img = imread('imgs/foliage raw.tiff');
 img = double(img) / 4096.0;
+final_img = demosaicBayer(img);
 
-[m, n] = size(img);
-blue_mask = zeros(m, n);
-blue_mask(2:2:end, 2:2:end) = 1;
-
-green_mask = zeros(m, n);
-green_mask(2:2:end, 1:2:end) = 1;
-green_mask(1:2:end, 2:2:end) = 1;
-
-red_mask = zeros(m, n);
-red_mask(1:2:end, 1:2:end) = 1;
-
-% get color contribution
-red_contribution = img.*red_mask;
-green_contribution = img.*green_mask;
-blue_contribution = img.*blue_mask;
-
-I3x3 = ones(3);
-interpolated_red_contribution = conv2(red_contribution, I3x3, 'same');
-interpolated_green_contribution = conv2(green_contribution, I3x3, 'same');
-interpolated_blue_contribution = conv2(blue_contribution, I3x3, 'same');
-
-
-norm_red = interpolated_red_contribution ./ conv2(red_mask, I3x3, 'same');
-norm_green = interpolated_green_contribution ./ conv2(green_mask, I3x3, 'same');
-norm_blue = interpolated_blue_contribution ./ conv2(blue_mask, I3x3, 'same');
-
-
-%red_channel = norm_red.*(green_mask+blue_mask)+red_contribution;
-%green_channel = norm_green.*(red_mask+blue_mask)+green_contribution;
-%blue_channel = norm_blue.*(green_mask+red_mask)+blue_contribution;
-
-red_channel = (norm_red.* ~red_mask) + red_contribution;
-green_channel = (norm_green.* ~green_mask) + green_contribution;
-blue_channel = (norm_blue.* ~blue_mask) + blue_contribution;
-
-color_pile = [red_channel green_channel blue_channel ];
-
-final_img = reshape(color_pile,m,n,3);
 imshow(final_img);
 
 
@@ -69,8 +32,118 @@ imshow(final_img);
 % Proceed similarly...
 
 %% Assignement 2.2
+% we are interested in red green and blue green differences
+% thus
+% transform into y u v colors space
+img = imread('imgs/black and white raw.tif');
+img = double(img) / 255.0;
+
+[final_img, red_c, green_c, blue_c] = demosaicBayer(img);
+yuv_final = rgb2yuv(final_img);
+
+% median filtered imgs
+med_U = medfilt2(yuv_final(:,:,2));
+med_V = medfilt2(yuv_final(:,:,3));
+
+rgb2yuv_transform = [0.299 0.587 0.114; 
+                     -0.14713 -0.28886 0.436; 
+                      0.615 -0.51499 -0.10001];
+                  
+                  
+
+                  
+%red part
+red_known = rgb2yuv_transform(2:end, 2:end);
+red_v = rgb2yuv_transform(2:end, 1);
+red = red_c;%final_img(:,:,1);
+U_red = red_v(1)*red;
+V_red = red_v(2)*red;
+
+del_U = med_U-U_red;
+del_V = med_V-V_red;
+A = reshape([del_U(:),del_V(:)], 1000, 500)';
+rr = red_known\[del_U(:),del_V(:)]';
+% mask it for red channel
+
+serialized_red_mask = red_mask(:)';
+
+abc = [red(:)'.*serialized_red_mask; rr(1,:).*serialized_red_mask; rr(2,:).*serialized_red_mask];
+ttt = reshape(abc', 500, 500, 3);
 
 
+%blue
+red = blue_c;%final_img(:,:,3);
+red_known = rgb2yuv_transform(2:end, 1:end-1);
+red_v = rgb2yuv_transform(2:end, 3);
+
+U_red = red_v(1)*red;
+V_red = red_v(2)*red;
+
+del_U = med_U-U_red;
+del_V = med_V-V_red;
+A = reshape([del_U(:),del_V(:)], 1000, 500)';
+rr = red_known\[del_U(:),del_V(:)]';
+% mask it for red channel
+    blue_mask = zeros(500, 500);
+    blue_mask(2:2:end, 2:2:end) = 1;
+serialized_red_mask = blue_mask(:)';
+
+
+
+abc = [red(:)'.*serialized_red_mask; rr(1,:).*serialized_red_mask; rr(2,:).*serialized_red_mask];
+ttt = ttt + reshape(abc', 500, 500, 3);
+
+
+
+% green
+red = green_c;%final_img(:,:,2);
+red_known = rgb2yuv_transform(2:end, 1:2:end);
+red_v = rgb2yuv_transform(2:end, 2);
+
+U_red = red_v(1)*red;
+V_red = red_v(2)*red;
+
+del_U = med_U-U_red;
+del_V = med_V-V_red;
+A = reshape([del_U(:),del_V(:)], 1000, 500)';
+rr = red_known\[del_U(:),del_V(:)]';
+% mask it for red channel
+    green_mask = zeros(500, 500);
+    green_mask(2:2:end, 1:2:end) = 1;
+    green_mask(1:2:end, 2:2:end) = 1;
+serialized_red_mask = green_mask(:)';
+
+
+
+abc = [red(:)'.*serialized_red_mask; rr(1,:).*serialized_red_mask; rr(2,:).*serialized_red_mask];
+ttt = ttt + reshape(abc', 500, 500, 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+imshow(ttt)
+% generalize this for other channels
+% sum up
+% reshape
 %% Assignement 2.3
 
 
