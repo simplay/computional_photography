5723ba79bea2a15dcbd1ac2205ce75b9ca74c4b6
simplay <michael.single@students.unibe.ch>
2014-10-01 15:54:36 +0200

    Displaying legend of curves in plots
    using different random colors for curves.

--- plotGamma.m
+++ plotGamma.m
@@ -1,9 +1,13 @@
 function plotGamma( Lin, Lout, gamma )
 %PLOTGAMMA Summary of this function goes here
 %   Detailed explanation goes here
-    plot(Lin(:),Lout(:), '.');
+    graphColor = rand(1,3);
+    plot(Lin(:),Lout(:), '.', 'color', graphColor);
     xlabel('Input Intensitiy');
     ylabel('Output Intensitiy');
     title('foobar')
+    set(gcf,'Color',graphColor)
+    str = strcat('gamma=', + num2str(gamma));
+    legend(str);
 end
 
