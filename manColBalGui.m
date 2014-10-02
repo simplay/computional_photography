12bcdbfdff5221d1084aff1afbc3ea3ce404853d
simplay <michael.single@students.unibe.ch>
2014-10-02 02:15:13 +0200

    Fixed minor code-smell: Missing semicolon

--- manColBalGui.m
+++ manColBalGui.m
@@ -1,7 +1,7 @@
 function [ out ] = manColBalGui( img , idx)
 %MANCOLBALGUI Summary of this function goes here
 %   Detailed explanation goes here
-    [x,y]=ginput(1)
+    [x,y]=ginput(1);
     x = round(x);
     y = round(y);
     out = manualWhiteBalance(img, [x,y]);
