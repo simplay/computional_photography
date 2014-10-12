6fcb116f7619c25fc9ef4072191a1d44cf4c98d7
simplay <michael.single@students.unibe.ch>
2014-10-12 21:40:41 +0200

    Further love to hdr tone mapper
    working in correct color-domains :)

--- hdrToneMapping.m
+++ hdrToneMapping.m
@@ -1,6 +1,6 @@
 function [ out ] = hdrToneMapping( output_range )
-    hdrImg = hdrread('imgs/memorial_o876.hdr');
-    eps = 0.00001;
+    hdrImg = hdrread('imgs/dani_belgium_oC65.hdr');
+    eps = 0.0001;
     R = hdrImg(:,:,1);
     G = hdrImg(:,:,2);
     B = hdrImg(:,:,3);
@@ -12,19 +12,19 @@ function [ out ] = hdrToneMapping( output_range )
     g = (G ./ intensities);
     b = (B ./ intensities);
     
+    log_intensities = log(intensities);
     
-    
-    log_base = bfilt(log(intensities), 2, 0.12);
-    log_detail = log(intensities) - log_base;
+    log_base = bfilt(log_intensities, 2, 0.12);
+    log_detail = log_intensities - log_base;
     
     compressionF = log(output_range)/(max(log_base(:))-min(log_base(:)));
     log_offset = -max(log_base(:))*compressionF;
     
     log_output_intensitiy = log_base*compressionF + log_offset+log_detail;
     
-    R_output = r.*exp(log_output_intensitiy);
-    G_output = g.*exp(log_output_intensitiy);
-    B_output = b.*exp(log_output_intensitiy);
+    R_output = r .* exp(log_output_intensitiy);
+    G_output = g .* exp(log_output_intensitiy);
+    B_output = b .* exp(log_output_intensitiy);
     
        
     out = mat2Img(R_output, G_output, B_output );
