484b928bb93014c4218268cbe091741b0e8e704f
simplay <michael.single@students.unibe.ch>
2014-10-12 18:55:01 +0200

    Added progress bar for bfilter

--- bfilt.m
+++ bfilt.m
@@ -1,6 +1,9 @@
 function [ out ] = bfilt( img, sigma_s, sigma_r, w )
 %BFILT Summary of this function goes here
 %   Detailed explanation goes here
+    h = waitbar(0, 'Applying Bilateral Filter...');
+    set(h, 'Name', 'Bilateral Filter Progress Bar');
+
     [m, n, p] = size(img);
     out = zeros(m,n);
     for i = 1:m,
@@ -18,7 +21,8 @@ function [ out ] = bfilt( img, sigma_s, sigma_r, w )
             EV = exp(DeltaNValues+deltaNIdx);
             out(i,j) = (EV(:)'*neighboordhoodValues(:))/sum(EV(:));
         end
+        waitbar(i/m);
     end
-
+    close(h);
 end
 
