ac31df4847b66369f2557f30133108f0a020fa81
simplay <michael.single@students.unibe.ch>
2014-10-12 22:34:08 +0200

    Implemented dummy header for tone adjustment

---
+++ automatedToneAdjustment.m
@@ -0,0 +1,9 @@
+function [ output_args ] = automatedToneAdjustment( input, model, scaleF )
+%awesomify given input images refering to a given awesome model image.
+%  images which can be considered as rather being boring.
+%  model images which is suppoes to be awesome.
+%  scaleF enhancement factor, real numbered value.
+
+
+end
+
