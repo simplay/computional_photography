b4913f66f7b84747fe24e328d9166d5b98ca9ddd
simplay <michael.single@students.unibe.ch>
2014-10-14 17:47:47 +0200

    Paramtererized labeling + removed echos

--- demoBilateralFilter.m
+++ demoBilateralFilter.m
@@ -1,25 +1,45 @@
 function demoBilateralFilter( img, sigma_s_seq, sigma_r_seq )
-%DEMOBILATERALFILTER Summary of this function goes here
-%   Detailed explanation goes here
+%DEMOBILATERALFILTER renders bilateral filterd images, each in a subplot for
+%each sigma value combination.
+    
+    % open new figure
+    figure_handler = figure;
 
     % used for later for loop in order to generate the figures.
     len_sig_s = length(sigma_s_seq);
     len_sig_r = length(sigma_r_seq);
     
-    
+    % get all sigma value combinations
     sigma_pairs = arrays2pairs(sigma_s_seq, sigma_r_seq);
     
     hold on
     idx = 1;
     for k_s=1 : len_sig_s,
         for k_r=1 : len_sig_r,
+            
+            % combunte current bilateral filter img.
             sigmas = sigma_pairs(idx, :);
             img = bilateralFilter(img, sigmas(1), sigmas(2));
-            subplot(len_sig_s,len_sig_r, idx), subimage(img)
+            
+            % assign idx of subfigure handler
+            g = subplot(len_sig_s,len_sig_r, idx); 
+            subimage(img);
+
+            % parse label text (used sigma values) for current subfigure. 
+            label_text = strcat(' sigma r=',num2str(sigmas(1)), ...
+                       ' sigma r=',num2str(sigmas(2)));
+            
+            % remove coordiante axis for plots
+            set(gca,'xtick',[],'ytick',[]);
+            
+            % set x-axis label for current figure
+            xlabelHandler = get(g,'XLabel');
+            set( xlabelHandler, 'String', label_text);
+            
+            % update global image index
             idx = idx + 1;
         end
     end
-
-
+    hold off
 end
 
