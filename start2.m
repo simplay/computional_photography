4bbc5704204a074f3f9e42a137bfd2f027d032f3
simplay <michael.single@students.unibe.ch>
2014-10-14 18:25:10 +0200

    Modified start2 script according to latest code basis

--- start2.m
+++ start2.m
@@ -1,12 +1,14 @@
 %% Task2
 % bilateral filter
-img = imread('imgs/Bigel11.jpg');
+%img = imread('imgs/Bigel11.jpg');
+img = imread('imgs/rock.png');
 img2 = double(img)/255;
-a = 3.0; b = 0.1;
 
-figure
-out = bfiltImg3(img2, a, b);
-imshow(out);
+sigma_s = [2, 6, 18];
+sigma_r = [0.1, 0.25, 10000];
+
+demoBilateralFilter(img2(:,:,1), sigma_s, sigma_r);
+
 
 %figure(1)
 %imshow(img3)
@@ -21,16 +23,16 @@ imshow(out);
 %imshow(hdrToneMapping(hdrImg,30))
 
 % tone adjustment - awesomification
-awesomeImg = imread('imgs/winterstorm.png');
-awesomeImg = double(awesomeImg)/255;
-baseImg = imread('imgs/rock.png');
-baseImg = double(baseImg)/255;
+%awesomeImg = imread('imgs/winterstorm.png');
+%awesomeImg = double(awesomeImg)/255;
+%baseImg = imread('imgs/rock.png');
+%baseImg = double(baseImg)/255;
 
-advOut = toneAdjustmentColorImg(baseImg, awesomeImg, 1.0);
-simpleOut = simpleautoToneAdjustment(baseImg, awesomeImg);
+%advOut = toneAdjustmentColorImg(baseImg, awesomeImg, 1.0);
+%simpleOut = simpleautoToneAdjustment(baseImg, awesomeImg);
 
-figure(1000)
-imshow(advOut)
+%figure(1000)
+%imshow(advOut)
 
-figure(1001)
-imshow(simpleOut)
+%figure(1001)
+%imshow(simpleOut)
