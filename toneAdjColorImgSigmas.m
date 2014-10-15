3f82f6a14c0b9f0376dac7adfaf62f14b0cff302
simplay <michael.single@students.unibe.ch>
2014-10-15 03:07:29 +0200

    Added documentation for sigma series tone adjustment

--- toneAdjColorImgSigmas.m
+++ toneAdjColorImgSigmas.m
@@ -1,17 +1,27 @@
 function [ out ] = toneAdjColorImgSigmas( baseImg, awesomeImg, scaleF, sigma_s_seq, sigma_r_seq )
-%TONEADJCOLORIMGSIGMAS Summary of this function goes here
-%   Detailed explanation goes here
-
+%TONEADJCOLORIMGSIGMAS tone mapping using a bilateral filter
+%   for a series of sigma values. also applying a detail scale factor.
+%   the tone of the baseImg is matched to the tone histogram of the awesome
+%   img.
+%   @return tensor containing all computed color images.
+    
+    % colorspace transformation into yuv space.
     yuv_base = rgb2yuv(baseImg);
     yuv_awesome = rgb2yuv(awesomeImg);
     
+    % form all sigma pairs
     sigma_pairs = arrays2pairs(sigma_s_seq, sigma_r_seq);
+    
+    % get dimensions and lengths of paramter spaces.
     len_sig_s = length(sigma_s_seq);
     len_sig_r = length(sigma_r_seq);
-    
     [m,n] = size(yuv_base(:,:,1));
     
-    out = zeros(m,n, 3, len_sig_s*len_sig_r);
+    % prepare output tensor:
+    % there are len_sig_s*len_sig_r (m x n) color images (i.e. 3 channels).
+    out = zeros(m, n, 3, len_sig_s*len_sig_r);
+    
+    % perform computation for each sigma pair
     for k=1:len_sig_s*len_sig_r,
         sigmas = sigma_pairs(k, :);
         toneAdjustedY = automatedToneAdjustment(yuv_base(:,:,1), ...
@@ -20,6 +30,5 @@ function [ out ] = toneAdjColorImgSigmas( baseImg, awesomeImg, scaleF, sigma_s_s
         out(:,:,:,k) = yuv2rgb(mat2Img(toneAdjustedY, yuv_base(:,:,2), yuv_base(:,:,3)));                     
     end
     
-
 end
 
