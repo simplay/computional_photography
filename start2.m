7eb4bee7b160dcd0fdc4f1706d3bcc8d6dcd6967
simplay <michael.single@students.unibe.ch>
2014-10-15 02:37:59 +0200

    Modified the starting script

--- start2.m
+++ start2.m
@@ -1,13 +1,17 @@
 %% Task2
 % bilateral filter
-%img = imread('imgs/Bigel11.jpg');
-img = imread('imgs/rock.png');
+img = imread('imgs/Bigel11.jpg');
+%img = imread('imgs/rock.png');
 img2 = double(img)/255;
 
 sigma_s = [2, 6, 18];
 sigma_r = [0.1, 0.25, 10000];
+% red channel of rocks imgs
+% demoBilateralFilter(img2(:,:,1), sigma_s, sigma_r);
 
-demoBilateralFilter(img2(:,:,1), sigma_s, sigma_r);
+% bilat. filter luminance channel of rocks. 
+yuvImg = rgb2yuv(img2);
+%demoBilateralFilter(yuvImg(:,:,1), sigma_s, sigma_r);
 
 
 %figure(1)
@@ -23,16 +27,16 @@ demoBilateralFilter(img2(:,:,1), sigma_s, sigma_r);
 %imshow(hdrToneMapping(hdrImg,30))
 
 % tone adjustment - awesomification
-%awesomeImg = imread('imgs/winterstorm.png');
-%awesomeImg = double(awesomeImg)/255;
-%baseImg = imread('imgs/rock.png');
-%baseImg = double(baseImg)/255;
+awesomeImg = imread('imgs/forest.jpg');
+awesomeImg = double(awesomeImg)/255;
+baseImg = imread('imgs/lake.png');
+baseImg = double(baseImg)/255;
 
-%advOut = toneAdjustmentColorImg(baseImg, awesomeImg, 1.0);
-%simpleOut = simpleautoToneAdjustment(baseImg, awesomeImg);
+%advOut = toneAdjustmentColorImg(baseImg, awesomeImg, 1);
+simpleOut = simpleautoToneAdjustment(baseImg, awesomeImg);
 
 %figure(1000)
 %imshow(advOut)
 
-%figure(1001)
-%imshow(simpleOut)
+figure(1001)
+imshow(simpleOut)
