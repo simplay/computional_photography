0495a4b253e2291162add00dacae9ad96652d45b
simplay <michael.single@students.unibe.ch>
2014-10-28 15:38:43 +0100

    Added some minor documentation for task 1

--- start3.m
+++ start3.m
@@ -1,9 +1,17 @@
-%% task 1
-close all
+% Computational Photography Project 1
+% Turned in by <Michael Single>
+% Legi: 08-917-445
 
+close all
+clear all
+
+%% task 1
+
+% a) plot function f(x) = cos(?ox) with ?o = 2?k/N, x ? [0, 16]
+% and the sampled version f (n) frequency k = 1, . . . , 16.
+% note that ?o denotes the angular frequency.
 figure('name','Plots of cos(2pi*k*x/N) Plots');
 N = 16;
-
 idx = 1;
 for k=1:16
     h = subplot(4,4,idx);
@@ -15,13 +23,15 @@ for k=1:16
     idx = idx + 1;
 end
 
-% 2.b)
+% 2.b) Indicate the value of ?o and k at which f(x) hits the Nyquist frequency
 disp(['N = ', num2str(N), ' thus N+1 = ', num2str(N+1), ' samples are used.']);
 disp(['Hence, the sample rate f_s is equal to ', num2str(N), ' samples per time unit.']); 
 disp(['Since the  Nyquist frequency f_n is equal to 0.5*f_s']); 
 disp(['It follows f_n = ',num2str(N/2),' for N equal to ',num2str(N)]); 
-disp(['Since k denotes a frequency we can set k = f_n = ', num2str(N/2)]);
+disp(['Since k denotes a frequency we can set k = f_n = ', num2str(N/2), '(i.e. hit k hits Nyquist frequency)']);
 disp(['Then omega = 2*pi*k/N = 2*pi*(N/2)/N = pi']);
 
 
-%% list a matrix for all w0 k pairs
+%% task 2
+
+
