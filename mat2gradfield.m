b65f384acbef3c54db1b773e3b3ef8d56d064f3c
simplay <michael.single@students.unibe.ch>
2014-10-31 03:14:45 +0100

    Initial work on Project4
    implemented get gradient field helper

---
+++ mat2gradfield.m
@@ -0,0 +1,12 @@
+function [ dx, dy ] = mat2gradfield( matrix )
+%MAT2GRADFIELD compute the gradient field along each direction dx, dy of a
+%matrix.
+%   @param matrix is a [m x n] matrix
+%   @return dx a [(m-1) x n] matrix
+%   @return dy a [m x (n-1)] matrix
+
+    dy = matrix(:, 2:end) - matrix(:, 1:end-1);
+    dx = matrix(2:end,:) - matrix(1:end-1,:);
+
+end
+
---
+++ start4.m
@@ -0,0 +1,6 @@
+img = imread('imgs/rock.png');
+img = im2double(img);
+yuvImg = rgb2yuv(img);
+yImg = yuvImg(:,:,1);
+dy = yImg(:, 2:end) - yImg(:, 1:end-1);
+dx = yImg(2:end,:) - yImg(1:end-1,:);
