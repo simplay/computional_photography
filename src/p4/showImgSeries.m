a1986c41031095aadaa51226fa10c82134058473
simplay <michael.single@students.unibe.ch>
2014-11-04 00:46:06 +0100

    Documented auxiliary function for displaying input data sets.

--- src/p4/showImgSeries.m
+++ src/p4/showImgSeries.m
@@ -1,12 +1,14 @@
 function showImgSeries(figureTitle, imgs, labels)
-%SHOWIMGSERIES Summary of this function goes here
-%   Detailed explanation goes here
+%SHOWIMGSERIES Display Subfigure series of a series of images with labels.
+%   @param figrueTitle string
+%   @param imgs a (m x n x 3 x k) tensor storing 
+%          k (m x n) color images (i.e. they have 3 color channels).
+%          assumption: all images have same dimensionality.
+%   @param labels a cell-grid storing k subfigure label titles (strings).
     figure('Position', [100, 100, 1024, 800], ...
            'name', figureTitle)
        
-       
-       % char(labels(2))
-       
+    % display subfigures using provided data.   
     for k=1:size(imgs,4),
         g = subplot(1,size(imgs,4), k);
         subimage(imgs(:,:,:,k))
