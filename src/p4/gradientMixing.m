409880a4265f20d32dcb18aa7d645c7da121665f
simplay <michael.single@students.unibe.ch>
2014-11-03 21:39:44 +0100

    Implemented initial version of Gradient Mixing
    + added some demo images

---
+++ src/p4/gradientMixing.m
@@ -0,0 +1,20 @@
+function [ out ] = gradientMixing( target, source, mask)
+%GRADIENTMIXING Summary of this function goes here
+%   Detailed explanation goes here
+
+    M = size(target,1); N = size(target,2);
+    out = zeros(M,N,3);
+
+    gradFieldTarget = img2gradfield(target);
+    gradFieldSource = img2gradfield(source);
+    gradField = max(gradFieldTarget, gradFieldSource);
+    
+    tic
+    parfor k=1:3
+        out(:,:,k) = poissonSolver(target(:,:,k), gradField(:,:,:,k), mask(:,:,k));
+    end
+    toc
+    
+    out = mat2Img(out(:,:,1),out(:,:,2),out(:,:,3));
+end
+
