26c19330cefa095b3530b5636109c0d6d70f1cf2
simplay <michael.single@students.unibe.ch>
2014-11-26 02:35:49 +0100

    Restructured code of task 1
    exported related code to its own script
    TODO: docs + some further refactoring

---
+++ src/p5/makeMorphingVideo.m
@@ -0,0 +1,66 @@
+function makeMorphingVideo( source, target, frameCount )
+%MAKEMORPHINGVIDEO Summary of this function goes here
+%   Detailed explanation goes here
+% properties
+fileName = 'morph1';
+matFile = strcat('data/p5/',fileName,'.mat');
+videoFile = strcat('outputs/p5/',fileName,'.avi');
+videoDuration = 1;
+selectNewPoints = false;
+timestepFn = @(t) t;
+
+
+% Start Control Point Selection tool with images and control points 
+% stored in variables in the workspace.
+[M,N,~] = size(source);
+
+if exist(matFile,'file') && ~selectNewPoints
+    load(matFile)
+else
+    % (x,y) points i.e. (column-idx,row-idx)
+    sourcePoints = [1,1; N,1; 1,M; N,M];
+    targetPoints = [1,1; N,1; 1,M; N,M];
+
+    % Ask cpselect to wait for you to pick some more points
+    
+    % If set to FALSE (the default) you can
+    % run cpselect at the same time as you run other 
+    % programs in MATLAB.
+    [sourcePoints, targetPoints] = cpselect(source, target, ...
+                                            sourcePoints, targetPoints, ...
+                                            'Wait', true);
+    % save points
+    save(matFile,'sourcePoints','targetPoints');
+    
+    
+    
+end
+
+% x values used in order to compute the timesteps t
+xs = linspace(0, 1, frameCount);
+morphedFrames = zeros(M,N,3,frameCount);
+morphedFrames(:,:,:,1) = source;
+morphedFrames(:,:,:,frameCount) = target;
+parfor k = 2:frameCount-1,
+    t = timestepFn(xs(k));
+    morphedFrames(:,:,:,k) = morph(source, target, sourcePoints, targetPoints, t);
+    disp(['frame number ',num2str(k), ' computed']);
+end
+
+%%
+% create video file from a series of frames.
+    fps = frameCount/videoDuration;
+    vidObj = VideoWriter(videoFile);
+    vidObj.FrameRate = fps;
+    open(vidObj);
+    for k = 1:frameCount
+        frame = im2double(morphedFrames(:,:,:,k));
+        frame = frame - min(frame(:));
+        frame = frame ./ max(frame(:));
+        frame = im2frame(frame);
+        writeVideo(vidObj,frame);
+    end
+    close(vidObj);
+
+end
+
--- start5.m
+++ start5.m
@@ -7,78 +7,29 @@ clear all;
 close all;
 
 % load dependencies
+% uncomment the next code line in case you have not installed vlfeat yet.
+% run lib/vlfeat-0.9.19/toolbox/vl_setup 
+
 addpath('util/');
+addpath('lib/');
 addpath('src/p5/');
 
-% properties
-fileName = 'morph1';
-matFile = strcat('data/p5/',fileName,'.mat');
-videoFile = strcat('outputs/p5/',fileName,'.avi');
-videoDuration = 1;
-selectNewPoints = false;
-frameCount = 20;
-timestepFn = @(t) t;
-
+%% Task 1: Morphing
 
 % load target and source images used for morphing
+numberOfFrames = 3;
+
 target = imread('imgs/p5/dude.jpg');
 target = im2double(target);
 
 source = imread('imgs/p5/pig.jpg');
 source = im2double(source);
 source = imresize(source, [size(target,1),size(target,2)]);
+    
+makeMorphingVideo(source, target, numberOfFrames)   
+
+%% Task 2
 
 
-% Start Control Point Selection tool with images and control points 
-% stored in variables in the workspace.
-[M,N,~] = size(source);
+%% Task 3
 
-if exist(matFile,'file') && ~selectNewPoints
-    load(matFile)
-else
-    % (x,y) points i.e. (column-idx,row-idx)
-    sourcePoints = [1,1; N,1; 1,M; N,M];
-    targetPoints = [1,1; N,1; 1,M; N,M];
-
-    % Ask cpselect to wait for you to pick some more points
-    
-    % If set to FALSE (the default) you can
-    % run cpselect at the same time as you run other 
-    % programs in MATLAB.
-    [sourcePoints, targetPoints] = cpselect(source, target, ...
-                                            sourcePoints, targetPoints, ...
-                                            'Wait', true);
-    % save points
-    save(matFile,'sourcePoints','targetPoints');
-    
-    
-    
-end
-
-% x values used in order to compute the timesteps t
-xs = linspace(0, 1, frameCount);
-morphedFrames = zeros(M,N,3,frameCount);
-morphedFrames(:,:,:,1) = source;
-morphedFrames(:,:,:,frameCount) = target;
-parfor k = 2:frameCount-1,
-    t = timestepFn(xs(k));
-    morphedFrames(:,:,:,k) = morph(source, target, sourcePoints, targetPoints, t);
-    disp(['frame number ',num2str(k), ' computed']);
-end
-
-%%
-% create video file from frame series.
-    fps = frameCount/videoDuration;
-    vidObj = VideoWriter(videoFile);
-    vidObj.FrameRate = fps;
-    open(vidObj);
-    for k = 1:frameCount
-        frame = im2double(morphedFrames(:,:,:,k));
-        frame = frame - min(frame(:));
-        frame = frame ./ max(frame(:));
-        frame = im2frame(frame);
-        writeVideo(vidObj,frame);
-    end
-    close(vidObj);
-    
-    
