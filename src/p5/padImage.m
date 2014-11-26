ff223c2dfd96183af619b1f9b67cce41c6bddaf0
simplay <michael.single@students.unibe.ch>
2014-11-26 18:35:43 +0100

    Documented #padImage helper function

--- src/p5/padImage.m
+++ src/p5/padImage.m
@@ -1,14 +1,25 @@
 function paddedImg = padImage(img, prePadDims, postPaddedDimsOf)
-    % pads image in the direction 'pre' and then 'post'. 
+% PADIMAGE Fit dimensionality of panorama images that are supposed to be
+% stiched. Zeros pads a given image in the direction 'pre' and then 'post'. 
     % @param img image to zero pad
     % @param prePadDims image dimensions for pre zero padding
     % @param postPaddedDimsOf function handler that helps determining post
     %        zero padding dimensions for given image.
+    % @return a zeros padded 3-channel double image that has 
+    % 'a' prepended rows and 'b' prepended columns
+    % 'c' appended rows and 'd' prepended columns
+    % where ['a', 'b'] = [prePadDims(1), prePadDims(2)]
+    % and ['c', 'd'] =
+    %   [postPaddedDimsOf(paddedImg)(1), postPaddedDimsOf(paddedImg)(2)]
 
     % Pad before the first array element along each dimension .
+    % Arepend prePadDims(1) zero rows 
+    % and prepend prePadDims(2) zero columns
     paddedImg = padarray(img, prePadDims, 0, 'pre');
     
-    % Pads after the last array element along each dimension.
+    % Pads after the last array element along each dimension
+    % Append postPaddedDimsOf(paddedImg)(1) zero rows 
+    % and append postPaddedDimsOf(paddedImg)(2) zero columns
     paddedImg = padarray(paddedImg, postPaddedDimsOf(paddedImg), 0, 'post');
     
     % adjust image dimensions - binary mask should also be an image
